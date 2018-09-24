98ea69b055e442ee448d1c7d46fe4b01dda4d693
jefferson <jeff.otoni@gmail.com>
2018-09-24 14:47:26 -0300

    corrigindo path route

--- src/fileserver/main.go
+++ src/fileserver/main.go
@@ -1,10 +1,19 @@
+/*
+* Go Library (C) 2017 Inc.
+*
+* @project     FileServer
+* @package     main
+* @author      @jeffotoni
+* @size        23/09/2018
+ */
+
 package main
 
 //
 //
 //
 import (
-	"github.com/jeffotoni/fileserver/pkg/route"
+	"github.com/jeffotoni/fileserver/route"
 )
 
 //
